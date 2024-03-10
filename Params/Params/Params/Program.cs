17c4ca91d2a81e2b70ff546fe0445640169a4164
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-03-10 19:56:35 -0300

    First commit

---
+++ Params/Params/Params/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var nome = "João";
+        var nome2 = "Felipe";
+        ImprimirNomes(nome, nome2);
+
+        var nomes = new string[] { "Caio", "José", "Maria" };
+        ImprimirNomes(nomes);
+
+        Console.ReadKey();
+    }
+
+    static void ImprimirNomes(params string[] nomes)
+    {
+        foreach (var nome in nomes)
+        {
+            Console.WriteLine(nome);
+        }
+    }
+}
