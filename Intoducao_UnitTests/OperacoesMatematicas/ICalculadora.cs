f02560de90302bdcb54692c997be03ee8b7e2cff
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-03-19 19:13:59 -0300

    Apresentando os pacotes AutoFixture, Moq e FluentAssertions

---
+++ Intoducao_UnitTests/OperacoesMatematicas/ICalculadora.cs
@@ -0,0 +1,9 @@
+﻿namespace OperacoesMatematicas
+{
+    public interface ICalculadora
+    {
+        int Somar(int num1, int num2);
+        int Multiplicar(int num1, int num2);
+        int Subtrair(int num1, int num2);
+    }
+}
